e0ea60ea888c36030dd976376e44481318dee8c2
Anton Sigurðsson <anton.sigurdsson@gmail.com>
2013-03-03 15:33:10 +0000

    Margt og mikið
    Lengd lags, staða lags, lagalisti, next, prev virkar allt.
    Einnig virkar að klikka á li-item til að velja lag.

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -12,58 +12,76 @@
     return this.each( function( ) {
       
         // Fela default spilarann
-        $('#playermain ul').css('display', 'none');
+        //$('#playermain ul').css('display', 'none');
         $('#spilari').css('display', 'none');
+        $('#playermain ul').css('position', 'absolute');
+        $('#playermain ul').css('top', '200px');
         
         // Id sett við hvert li á lag 
         var $li = $('#playermain ul > li').attr('id', function(i){
-            return "lag" + (i);
+            return  (i);
         });
         
-        // Lagalisti búinn til
-        var songlist = $li.toArray();
+        // Upphafsstaða spilunar 
+        var playing = pluginOptions.autoPlay;
+        if(playing == "true") {
+            var autop = "autoplay";
+        }
+        else {
+            var autop = "";
+        }
         
         // Upphafsnúmer lags        
         var current_song = 0;
         
         // Náð í id spilandi lags
-        var hl = "#lag" + current_song;
+        var hl = "#" + current_song;
         console.log(hl);
         var hlf = $(hl);
         
+        // Lagalisti búinn til
+        var songlist = $li.toArray();
         
         // Spilari búinn til og sendur
-        var player = $("<audio autoplay='autoplay' controls='controls' id='spilari'>");    
-        for(var i=0; i<songlist.length; i++){
-            $(player).append("<source id='lag" + i + "' src='" + songlist[i].textContent + "' />");
+        var player = $("<audio " + autop + " controls='controls' id='spilari'>");
+        $(player).append("<source src='" + songlist[current_song].textContent + "' />");
+        /////////////////----    Gamla ----//////////////
+        // for(var i=0; i<songlist.length; i++){
+            // $(player).append("<source id='lag" + i + "' src='" + songlist[i].textContent + "' />");
             
-            hlf.css({'background-color':'yellow'});
-        }
+            // hlf.css({'background-color':'yellow'});
+        // }
+        /////////////////////////////////////////////////
         player.append("</audio>");
         $(this).append(player);
-
+        
         // Spilarinn geymdur í breytu
-        var spilarinn = document.getElementById('spilari');
-        console.log(player);
-        // Upphafsstaða spilara
-        var playing = pluginOptions.autoPlay;
+        var spilarinn = document.getElementById('spilari');    
         
-        var refresh_timer = 1000;
-               
-        $(this).append(  '<div id="oval">'                           +
-                        '<div id="mpPlay" class="button"></div>'    +
-                        '<div id="mpList" class="button"></div>'    +
-                        '<div id="mpNext" class="button"></div>'    +
-                        '<div id="mpPrev" class="button"></div>'    +
-                        '<div id="mpVolUp" class="button"></div>'   +
-                        '<div id="mpVolDn" class="button"></div>'   +
-                        '</div>'                                    +
-                        '<div id="list"><div id="listbottom"></div></div>'
-        
-        
+        // Divum bætt við í spilarabreytu
+        $(this).append(  
+            '<div id="oval">'                                   +
+            '<div id="mpPlay" class="button"></div>'            +
+            '<div id="mpList" class="button"></div>'            +
+            '<div id="mpNext" class="button"></div>'            +
+            '<div id="mpPrev" class="button"></div>'            +
+            '<div id="mpVolUp" class="button"></div>'           +
+            '<div id="mpVolDn" class="button"></div>'           +
+            '<div id="mpCurrLen" class="button"></div>'         +
+            '<div id="mpCurrPos" class="button"></div>'         +
+            '</div>'                                            +
+            '<div id="list"><div id="listbottom"></div></div>'
         );
+        
+        // Klikkað á lag í lagalista
+        $('#playermain ul li').click(function() {
+            spilarinn.pause();
+            current_song = parseInt($(this).attr('id'));
+            $(player).html("<source src='" + songlist[current_song].textContent + "' />");
+            spilarinn.play();     
+        });
 
-        // Play/Pause takka virkni
+        // Play-Pause takka virkni
         $("#mpPlay").click( function() { 
             if(!playing) {
                 spilarinn.play();
@@ -74,6 +92,8 @@
                 playing = false;
             }
         });
+        
+        // Show-Hide lagalista
         $("#mpList").click(function(){
            $("#list").fadeToggle(1500);
         });
@@ -88,86 +108,75 @@
                 spilarinn.volume += 0.1;
             }
         });
+        
         // Volume down virkni
         $("#mpVolDn").click( function() { 
             if(spilarinn.volume < 0.1) {
                 spilarinn.volume = 0;
-                console.log(spilarinn.volume);
             }
             else {
                 spilarinn.volume -= 0.1;
-                console.log(spilarinn.volume);            
             }    
         });
         
         // Next virkni
         $("#mpNext").click( function() {
-             spilarinn.pause();
-             //spilarinn.src = songlist[1];
-             spilarinn.play();
+            spilarinn.pause();
 
-
-         });
-
-            
-           
-
-            //spilarinn.pause();
-            //console.log(songlist[current_song]);
-            // spilarinn.currentSrc = '';
-            //current_song++;
-            //console.log(songlist[current_song].url);
-            //spilarinn.src = songlist[current_song].url;
-            //spilarinn.play();
-            //var nextsong = spilarinn.next();
-            //console.log(spilarinn);
-            //nextsong.play();
-            
-
-            
-        //});
+            if(current_song < songlist.length - 1) {
+                current_song++;             
+            }
+            else {
+                current_song = 0;
+            }
+            $(player).html("<source src='" + songlist[current_song].textContent + "' />");
+            spilarinn.play();
+        });
         
-        // mpCurrent virkni
-        var lengd_lags = this.duration;
-        var stada_lags = this.currenttime;
-        $("#mpCurrent").html(lengd_lags);
-
-        ////// Gamla Listinn af lögum ////////
-        // function showSongList(highlight){
-            // var mylist = $("<div id='lagalisti'></div>");
-            // for (var i=0; i<songlist.length; i++) {
-                // $(mylist).append("<div id=lag" + i + ">" + songlist[i].textContent + "</div>");
-            // };
-
-            // //console.log(hlf);
-            // // console.log($("#mpPlay"));
-            // // var temp = hlf.innerHTML();
-            // var temp2 = $('#lag1')
-            // console.log(temp2);
-            // // console.log("temp er: " + temp);
-            // $('#lag1').css({'background-color':'yellow'});
-            // $("#mpSongList").html(mylist);
-        // }
-        // showSongList(current_song);
+        // Previous virkni
+        $("#mpPrev").click( function() {
+            spilarinn.pause();
+            if(current_song != 0) {
+                current_song--;
+            }
+            $(player).html("<source src='" + songlist[current_song].textContent + "' />");
+            spilarinn.play();
+        });
         
-        var refresh = setInterval(
-            function(){
-                //console.log(songlist.length);
-                var curr = songlist[current_song].textContent;
-                //console.log("current er: " + curr);
-
-                
-                // $("#mpCurrent").html(curr);
-                
-                    // for (var i=0; i<songlist.length; i++) {
-                        // $("#mpSongList").html(function() {
-                            // var thelist
-                        // // $(this).html('<div>' + songlist[i].textContent +'</div>');
-                        // // // console.log(songlist[i].textContent);
-                         // // // $(this).html('<div>' + i +'</div>');
-                        // });
-                    // }
-                    
+        // Tekur inn slóð og skilar heiti lags
+        function heitilags(s) {
+            
+        }
+        
+        // Tekur inn tímalengt í sekúndum og skilar strengi á forminu "m:ss"
+        function lagaTima(timi){
+            min = Math.floor(timi / 60);
+            sek = Math.floor(timi % 60);
+            if(sek < 10) {
+                sek = "0" + sek;            
+            }
+            return min + ":" + sek;
+        };
+        
+        spilarinn.addEventListener("ended", function() {
+            if(current_song < songlist.length-1) {
+                current_song++;
+                $(player).html("<source src='" + songlist[current_song].textContent + "' />");
+                spilarinn.play();
+            }
+        });
+        
+        // Uppfærir lifandi div í spilara
+        function updatePlayerStatus(stada, len) {            
+            $('#mpCurrPos').html(lagaTima(stada));
+            $('#mpCurrLen').html(lagaTima(len));
+        };
+        
+        // Uppfærir stöðu lifandi breyta
+        var refresh = setInterval( function(){
+                stada_lags = spilarinn.currentTime;
+                lengd_lags = spilarinn.duration;
+                updatePlayerStatus(stada_lags, lengd_lags);                   
             }, 1000);
             
       });
